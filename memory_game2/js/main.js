caf9e710fa913a7a3ad1cf0556cc07ad979e0a67
Nicole Auerbach <Nicole@Nicoles-MacBook-Air.local>
2018-02-23 18:41:28 -0800

    created game logic

--- memory_game2/js/main.js
+++ memory_game2/js/main.js
@@ -2,19 +2,21 @@ console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
+var checkForMatch = function() {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    } else {
+      alert("Sorry, try again.");
+    }
+  };
+};
 
-console.log("User flipper " + cardOne);
+var flipCard = function(cardId){
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+};
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again.");
-  }
-}
+flipCard(0);
+flipCard(2);
